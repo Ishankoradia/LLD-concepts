1e3b2a1d32fef1742b96e93b042b5d3c0aa47122
Ishankoradia <ikoradia@umich.edu>
2024-02-18 15:30:30 +0530

    copy constructor with shallow and deep copy

--- java_concepts/src/main/java/lld/Constructors/Node.java
+++ java_concepts/src/main/java/lld/Constructors/Node.java
@@ -1,5 +1,21 @@
 package lld.Constructors;
 
+/*
+ * Here is an example of "copy constructor" where you can create a new instance 
+ * from an object already created one in memory. 
+ */
 public class Node {
-    
+    // write the code of node class here
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node(Node nn) {
+        this.data = nn.data;
+        this.next = new Node(nn.next.data); // this is deep copying;
+        // this.next = nn.next; // this is shallow copy; where next of "this" & "nn" are pointing to the same location
+    }
 }
